34a417459aabd5c6a4664918e5a9d5060957cd65
Alex Petrenko <alex.pt@ya.ru>
2023-09-20 00:30:00 +0300

    Создать UI элементы в storybord, связать с View Controller

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -2,7 +2,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+// MARK: - Связываем UI элементы из сториборда с view controller
+    @IBOutlet weak var logTextView: UITextView!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var delButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
