dc9b3d04050bf93ea01084c07ca269775fa92eb1
Alex Petrenko <alex.pt@ya.ru>
2023-09-20 00:48:55 +0300

    Кастомизировать UI элементы в viewDidLoad

--- Counter/Counter/ViewController.swift
+++ Counter/Counter/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // MARK: - Кастомизируем UI (скругление углов, цвет кнопок, скролл для Text View):
+                addButton.layer.cornerRadius = 5
+                delButton.layer.cornerRadius = 5
+                resetButton.layer.cornerRadius = 5
+                resetButton.layer.borderWidth = 2
+                logTextView.layer.cornerRadius = 5
+                
+                addButton.backgroundColor = .red
+                delButton.backgroundColor = .blue
+                resetButton.layer.borderColor = UIColor.white.cgColor
+                
+                self.logTextView.setContentOffset(.zero, animated: true)
+                logTextView.text = "История изменений:\n\n"
+        
     }
 
 
